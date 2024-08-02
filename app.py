dc72113fe963b3d6ae183c5476e3f091d4db4138
souvikrnd <headoffice.test1@seple.in>
2024-08-02 16:31:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask,jsonify, request
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def api():
     if request.method == 'GET':
-        data={
+        data = {
             "message": "Hello"
         }
-    
-        return data
-    if request.method =='POST':
-        data = request.data
+        return jsonify(data)
+    elif request.method == 'POST':
+        data = request.json  # Assuming the data is sent in JSON format
         return jsonify(data=data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
